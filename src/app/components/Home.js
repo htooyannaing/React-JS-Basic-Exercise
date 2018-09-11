d6a621d867028dedab9b0a1c401581aa713a16ce
moethar15 <htooyannaingkyaw.cu@gmail.com>
2018-09-11 22:21:12 +0630

    props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 export class Home extends React.Component {
     render() {
         return(
             <div>
                 <p>Component</p>
+                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+                <p>User Object => Name: {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobbie, key) => {
+                            return <li key={key}>{hobbie}</li>
+                        })}
+                    </ul>
+                </div>
+                <hr/>
+                {/* {this.props.children.map((children, key) => {
+                    return children.type === 'p' ? children : ""
+                })} */}
+                <hr />
+                {this.props.children}
             </div>
         )
     }
+}
+
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    user: PropTypes.object,
+    children: PropTypes.element.isRequired
 }
